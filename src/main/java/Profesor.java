7d7d19274da78ef535db2b3c9b919eba0350484a
Agustin-PC\Agustin <agustin.ortiz.gonnet@gmail.com>
2020-07-29 20:22:26 -0300

    Profesor/practico7

---
+++ src/main/java/Profesor.java
@@ -0,0 +1,27 @@
+public class Profesor {
+
+    private String materia;
+    private float salario;
+
+    public Profesor() {
+
+    }
+
+    public String getMateria(){
+        return this.materia
+    }
+
+    public String setMateria (String unaMateria){
+        this.materia = unaMateria;
+    }
+
+    public float getSalario(){
+        return this.salario
+    }
+
+    public float setMateria (float unSalario){
+        this.salario = unSalario;
+    }
+
+    
+}
